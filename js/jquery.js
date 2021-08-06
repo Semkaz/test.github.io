26dcf0646c8d12db35cab55490b327a1f1c23e7d
Semkaz <kazach.semen@gmail.com>
2021-08-06 21:24:08 +0300

    добавил файлы

---
+++ js/jquery.js
@@ -0,0 +1,138 @@
+let intro = document.querySelector(".intro");
+let intro1 = document.querySelector(".intro1");
+let intro2 = document.querySelector(".intro2");
+let intro3 = document.querySelector(".intro3");
+let intro4 = document.querySelector(".intro4");
+let intro5 = document.querySelector(".intro5");
+let intro6 = document.querySelector(".intro6");
+let intro7 = document.querySelector(".intro7");
+let intro8 = document.querySelector(".intro8");
+let intro9 = document.querySelector(".intro9");
+let btn1 = document.querySelector(".btn1");
+let btn2 = document.querySelector(".btn2");
+let btn3 = document.querySelector(".btn3");
+let btn4 = document.querySelector(".btn4");
+let btn5 = document.querySelector(".btn5");
+let btn6 = document.querySelector(".btn6");
+let btn7 = document.querySelector(".btn7");
+let btn8 = document.querySelector(".btn8");
+let btn9 = document.querySelector(".btn9");
+
+
+
+btn1.onclick = function () {
+  intro.classList.remove("none");
+  intro1.classList.remove("none");
+  intro2.classList.add("none");
+  intro3.classList.add("none");
+  intro4.classList.add("none");
+  intro5.classList.add("none");
+  intro6.classList.add("none");
+  intro7.classList.add("none");
+  intro8.classList.add("none");
+  intro9.classList.add("none");
+};
+btn2.onclick = function () {
+  intro.classList.remove("none");
+  intro2.classList.remove("none");
+  intro1.classList.add("none");
+  intro3.classList.add("none");
+  intro4.classList.add("none");
+  intro5.classList.add("none");
+  intro6.classList.add("none");
+  intro7.classList.add("none");
+  intro8.classList.add("none");
+  intro9.classList.add("none");
+};
+btn3.onclick = function () {
+  intro.classList.remove("none");
+  intro3.classList.remove("none");
+  intro1.classList.add("none");
+  intro2.classList.add("none");
+  intro4.classList.add("none");
+  intro5.classList.add("none");
+  intro6.classList.add("none");
+  intro7.classList.add("none");
+  intro8.classList.add("none");
+  intro9.classList.add("none");
+
+};
+btn4.onclick = function () {
+  intro.classList.remove("none");
+  intro4.classList.remove("none");
+  intro2.classList.add("none");
+  intro3.classList.add("none");
+  intro1.classList.add("none");
+  intro5.classList.add("none");
+  intro6.classList.add("none");
+  intro7.classList.add("none");
+  intro8.classList.add("none");
+  intro9.classList.add("none");
+
+};
+btn5.onclick = function () {
+  intro.classList.remove("none");
+  intro5.classList.remove("none");
+  intro2.classList.add("none");
+  intro3.classList.add("none");
+  intro4.classList.add("none");
+  intro1.classList.add("none");
+  intro6.classList.add("none");
+  intro7.classList.add("none");
+  intro8.classList.add("none");
+  intro9.classList.add("none");
+
+};
+btn6.onclick = function () {
+  intro.classList.remove("none");
+  intro6.classList.remove("none");
+  intro2.classList.add("none");
+  intro3.classList.add("none");
+  intro4.classList.add("none");
+  intro5.classList.add("none");
+  intro1.classList.add("none");
+  intro7.classList.add("none");
+  intro8.classList.add("none");
+  intro9.classList.add("none");
+
+};
+btn7.onclick = function () {
+  intro.classList.remove("none");
+  intro7.classList.remove("none");
+  intro2.classList.add("none");
+  intro3.classList.add("none");
+  intro4.classList.add("none");
+  intro5.classList.add("none");
+  intro6.classList.add("none");
+  intro1.classList.add("none");
+  intro8.classList.add("none");
+  intro9.classList.add("none");
+
+};
+btn8.onclick = function () {
+  intro.classList.remove("none");
+  intro8.classList.remove("none");
+  intro2.classList.add("none");
+  intro3.classList.add("none");
+  intro4.classList.add("none");
+  intro5.classList.add("none");
+  intro6.classList.add("none");
+  intro7.classList.add("none");
+  intro1.classList.add("none");
+  intro9.classList.add("none");
+
+};
+btn9.onclick = function () {
+  intro.classList.remove("none");
+  intro9.classList.remove("none");
+  intro2.classList.add("none");
+  intro3.classList.add("none");
+  intro4.classList.add("none");
+  intro5.classList.add("none");
+  intro6.classList.add("none");
+  intro7.classList.add("none");
+  intro8.classList.add("none");
+  intro1.classList.add("none");
+
+};
+
---
+++ js/jquery1.js
@@ -0,0 +1,7 @@
+let button = document.querySelector('.btn_data');
+let btn = document.querySelector('.btn9');
+
+
+button.onclik= function () {
+
+}
